92e4549df5e3540ee165535b5fd7321b3ac8a468
pbostyn <72874454+pbostyn@users.noreply.github.com>
2021-03-22 14:26:49 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,150 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+source("Groupprofile.R")
+
+ui <- navbarPage("Covid cases and deaths for selected countries",
+                 tabPanel("Daily cases",
+
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput(inputId = "varcase",
+                                          "select countries",
+                                          choices=Countrycodes,
+                                          multiple=TRUE)
+                            ),
+                            # Show a plot 
+                            mainPanel(
+                              plotOutput(outputId = "casePlot")
+                            )
+        )
+    ),
+    
+                tabPanel("Daily deaths",
+             
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(inputId = "vardec",
+                             "select countries",
+                             choices=Countrycodes,
+                             multiple=TRUE)
+               ),
+               # Show a plot 
+               mainPanel(
+                 plotOutput(outputId = "dailydecPlot")
+               )
+             )
+    ),
+    
+              tabPanel("Confirmed cases",
+             
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(inputId = "varcasetot",
+                             "select countries",
+                             choices=Countrycodes,
+                             multiple=TRUE)
+               ),
+               # Show a plot 
+               mainPanel(
+                 plotOutput(outputId = "casetotPlot")
+               )
+             )
+    ),
+    
+            tabPanel("Confirmed deaths",
+             
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(inputId = "vardectot",
+                             "select countries",
+                             choices=Countrycodes,
+                             multiple=TRUE)
+               ),
+               # Show a plot 
+               mainPanel(
+                 plotOutput(outputId = "dectotPlot")
+               )
+             )
+    )
+    
+)
+
+# Define server logic 
+server <- function(input, output) {
+  
+  output$casePlot <- renderPlot({
+    
+    library(ggplot2)
+    library(ggthemes)
+    
+    
+    profile <- CountryGroupProfile(input$varcase)
+    
+    ggplot(data = profile) + aes(x = Date, y = DailyCases, color = CountryCode) +  
+      geom_line(size=1.15) + ylab("Daily cases / 100.000 inh.") +
+      ggtitle("Daily cases per 100.000 inh.") + 
+      scale_color_discrete(name="Country codes") +
+      scale_x_date(limits=as.Date(c("2021-01-01", NA)), date_breaks = "5 day",
+                   date_labels="%d / %m") + theme_solarized() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  output$dailydecPlot <- renderPlot({
+    library(ggplot2)
+    library(ggthemes)
+   
+     profile <- CountryGroupProfile(input$vardec)
+    
+    ggplot(data = profile) + aes(x = Date, y = DailyDeaths, color = CountryCode) +  
+      geom_line(size=1.15) + ylab("Daily deaths per million inh.") +
+      ggtitle("Daily deaths per million inh.") + 
+      scale_color_discrete(name="Country codes") +
+      scale_x_date(limits=as.Date(c("2021-01-01", NA)), date_breaks = "5 day",
+                   date_labels="%d / %m ") + theme_solarized() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  output$casetotPlot <- renderPlot({
+    library(ggplot2)
+    library(ggthemes)
+    
+    profile <- CountryGroupProfile(input$varcasetot)
+    
+    ggplot(data = profile) + aes(x = Date, y = ConfirmedCases, color = CountryCode) +  
+      geom_line(size=1.15) + ylab("Confirmed Cases / 100.000 inh.") +
+      ggtitle("Confirmed cases per 100.000 inh.") + 
+      scale_color_discrete(name="Country codes") +
+      scale_x_date(limits=as.Date(c("2020-03-01", NA)), date_breaks = "2 month",
+                   date_labels="%d / %m / %Y") + theme_solarized() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  
+  output$dectotPlot <- renderPlot({
+    library(ggplot2)
+    library(ggthemes)
+    
+    profile <- CountryGroupProfile(input$vardectot)
+    
+    ggplot(data = profile) + aes(x = Date, y = ConfirmedDeaths, color = CountryCode) +  
+      geom_line(size=1.15) + ylab("Confirmed deaths / 100.000 inh.") +
+      ggtitle("Confirmed deaths per 100.000 inh.") + 
+      scale_color_discrete(name="Country codes") +
+      scale_x_date(limits=as.Date(c("2020-03-01", NA)), date_breaks = "2 month",
+                   date_labels="%d / %m / %Y") + theme_solarized() +
+      theme(plot.title = element_text(hjust = 0.5))
+  })
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
